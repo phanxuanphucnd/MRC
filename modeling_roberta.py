ffb996ebb8ecd206c9d83dd406fae30a0fde5287
Phuc Phan <phanxuanphucnd@gmail.com>
2021-10-07 19:51:52 +0000

    update

--- modeling_roberta.py
+++ modeling_roberta.py
@@ -136,6 +136,7 @@ class RobertaForQuestionAnsweringSeqSC(RobertaPreTrainedModel):
         self.num_labels = config.num_labels
         self.roberta = RobertaModel(config)
         self.attention = SCAttention(config.hidden_size, config.hidden_size)
+        self.has_ans = nn.Sequential(nn.Dropout(p=config.hidden_dropout_prob), nn.Linear(config.hidden_size, 2))
         self.qa_outputs = nn.Linear(config.hidden_size, config.num_labels)
 
         self.init_weights()
@@ -163,6 +164,9 @@ class RobertaForQuestionAnsweringSeqSC(RobertaPreTrainedModel):
         start_logits = start_logits.squeeze(-1)
         end_logits = end_logits.squeeze(-1)
 
+        first_word = sequence_output[:, 0, :]
+        has_log = self.has_ans(first_word)
+
         outputs = (start_logits, end_logits,) + outputs[2:]
         if start_positions is not None and end_positions is not None:
             # If we are on multi-GPU, split add a dimension
@@ -170,15 +174,21 @@ class RobertaForQuestionAnsweringSeqSC(RobertaPreTrainedModel):
                 start_positions = start_positions.squeeze(-1)
             if len(end_positions.size()) > 1:
                 end_positions = end_positions.squeeze(-1)
+            if len(is_impossibles.size()) > 1:
+                is_impossibles = is_impossibles.squeeze(-1)
+
             # sometimes the start/end positions are outside our model inputs, we ignore these terms
             ignored_index = start_logits.size(1)
             start_positions.clamp_(0, ignored_index)
             end_positions.clamp_(0, ignored_index)
+            is_impossibles.clamp_(0, ignored_index)
 
             loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
             start_loss = loss_fct(start_logits, start_positions)
             end_loss = loss_fct(end_logits, end_positions)
-            total_loss = (start_loss + end_loss) / 2
+            choice_loss = loss_fct(has_log, is_impossibles)
+            total_loss = (start_loss + end_loss + choice_loss) / 3
+            
             outputs = (total_loss,) + outputs
 
         return outputs  # (loss), start_logits, end_logits, (hidden_states), (attentions)
