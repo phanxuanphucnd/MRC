4e68b026b49be3ee5ad40967e26eae30af61994f
Phuc Phan <phanxuanphucnd@gmail.com>
2021-10-20 07:33:48 +0000

    update

--- sh_run_av.sh
+++ sh_run_av.sh
@@ -1,28 +1,28 @@
 #Intensive module
 export SQUAD_DIR=data/uit-visquad
-export TRAIN_FILE=train-1.json
-export DEV_FILE=dev-1.json
+export TRAIN_FILE=train-0.json
+export DEV_FILE=dev-0.json
 export PREDICT_FILE=ptest.json
 python ./run_av.py \
-    --model_type xlm-roberta-dep2 \
-    --model_name_or_path xlm-roberta-large \
+    --model_type xlm-roberta \
+    --model_name_or_path ancs21/xlm-roberta-large-vi-qa \
     --do_train \
     --do_eval \
     --do_lower_case \
     --version_2_with_negative \
     --train_file $SQUAD_DIR/$TRAIN_FILE \
-    --predict_file $SQUAD_DIR/$PREDICT_FILE \
+    --predict_file $SQUAD_DIR/$DEV_FILE \
     --learning_rate 2e-5 \
-    --num_train_epochs 15 \
+    --num_train_epochs 10 \
     --max_seq_length 384 \
     --doc_stride 128 \
     --max_query_length=64 \
     --per_gpu_train_batch_size=8 \
     --per_gpu_eval_batch_size=16 \
     --warmup_steps=814 \
-    --output_dir models/uit-visquad/xlm-r-dep2-1 \
+    --output_dir models/uit-visquad/xlm-r-viqa-0 \
     --eval_all_checkpoints \
-    --save_steps 2500 \
-    --n_best_size=20 \
+    --save_steps 5000 \
+    --n_best_size=10 \
     --max_answer_length=50 \
     --overwrite_output_dir
